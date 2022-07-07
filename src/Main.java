e1d38c3636b035efdc93ff11e1815301321acc1d
zlohbierdcul <82916072+zlohbierdcul@users.noreply.github.com>
2022-07-07 02:53:14 +0200

    Main update

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,20 @@
-import javax.naming.NameNotFoundException;
-import java.util.ArrayList;
-
 public class Main {
 
-    public static void main(String[] args) throws NameNotFoundException {
-        Infomon bisa = new Infomon("bisasam", "earth", 100, 100);
-        Infomon knofi = new Infomon("knofensa", "earth", 100, 100);
+    public static void main(String[] args) {
+        Infomon bisa = new Infomon("bisasam", "grass", 101, 100);
+        Infomon knofi = new Infomon("knofensa", "grass", 100, 100);
         Infomon gluri = new Infomon("glumanda", "fire", 500, 500);
         Infomon schiggy = new Infomon("schiggy", "water", 1000, 300);
 
 
-//        System.out.println("Knof stronger than Bisa");
-//        System.out.println(infomon2.isStronger(infomon));
 
-
-        Trainer me = new Trainer("jager");
-        me.calculateTrainerPoints();
-        me.printTrainerPoints();
+        Trainer me = new Trainer("aaaaa");
 
         System.out.println(me.attemptToCatch(bisa));
         System.out.println(me.attemptToCatch(knofi));
         System.out.println(me.attemptToCatch(gluri));
 
-        System.out.println(bisa.isStronger(knofi));
+        System.out.println(bisa.getName() + " stronger " + knofi.getName() + "? | " + bisa.isStronger(knofi));
 
     }
 }
