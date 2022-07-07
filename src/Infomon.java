811cea51e532a3bdbe48a3d9ede8f758175e57be
zlohbierdcul <82916072+zlohbierdcul@users.noreply.github.com>
2022-07-07 02:51:01 +0200

    Infomon update
    added comments and fixed some minor bugs

--- src/Infomon.java
+++ src/Infomon.java
@@ -1,122 +1,108 @@
 import java.util.Objects;
 
 public class Infomon {
+    private final String myType, myName;
+    private int myAttackStat, myDefenceStat;
 
-    private String myName;
-    private String myType;
-    private int myAttackStat;
-    private int myDefenceStat;
-
-    public Infomon(String name, String type, int attackStat, int defenseStat) {
+    // Constructor sets the name, type, attackStat,
+    Infomon(String name, String type, int attackStat, int defenseStat) {
         this.myName = name;
         this.myType = type;
         this.myAttackStat = Math.max(attackStat, 0); // returns the greater of two int values
         this.myDefenceStat = Math.max(defenseStat, 0);
     }
 
+    // Getter methods
     public int getAttackStat() {
         return myAttackStat;
     }
-
-    public void setAttackStat(int attackStat) {
-        myAttackStat = attackStat;
-    }
-
     public int getDefenceStat() {
         return myDefenceStat;
     }
-
-    public void setDefenceStat(int defenceStat) {
-        myDefenceStat = defenceStat;
-    }
-
     public String getType() {
         return myType;
     }
+    public String getName() { return myName; }
 
+    // Checks if Infomon is stronger than enemy Infomon
     public boolean isStronger(Infomon enemy) {
-        int tempStat = myAttackStat;
-//        if (Objects.equals(myType, "fire") && Objects.equals(enemy.myType, "fire")) {
-//
-//        }
-        if (Objects.equals(myType, "fire") && Objects.equals(enemy.myType, "earth")) {
-            tempStat = myAttackStat * 2;
+        int tempAttack = getAttackStat();   // storing attackStat in temp variable so actual value isn't manipulated.
+
+        // Checking all different cases of effectiveness and changing the temporary attackStat accordingly
+        if (Objects.equals(getType(), "fire") && Objects.equals(enemy.getType(), "grass")) {
+            tempAttack *= 2;
         }
-        else if (Objects.equals(myType, "fire") && Objects.equals(enemy.myType, "water")) {
-            tempStat = myAttackStat / 2;
+        else if (Objects.equals(getType(), "fire") && Objects.equals(enemy.getType(), "water")) {
+            tempAttack /= 2;
         }
-//        if (Objects.equals(myType, "water") && Objects.equals(enemy.myType, "water")) {
-//
-//        }
-        else if (Objects.equals(myType, "water") && Objects.equals(enemy.myType, "fire")) {
-            tempStat = myAttackStat * 2;
+        else if (Objects.equals(getType(), "water") && Objects.equals(enemy.getType(), "fire")) {
+            tempAttack *= 2;
         }
-        else if (Objects.equals(myType, "water") && Objects.equals(enemy.myType, "earth")) {
-            tempStat = myAttackStat / 2;
+        else if (Objects.equals(getType(), "water") && Objects.equals(enemy.getType(), "grass")) {
+            tempAttack /= 2;
         }
-//        if (Objects.equals(myType, "earth") && Objects.equals(enemy.myType, "earth")) {
-//            myAttackStat = myAttackStat;
-//        }
-        else if (Objects.equals(myType, "earth") && Objects.equals(enemy.myType, "fire")) {
-            tempStat = myAttackStat / 2;
+        else if (Objects.equals(getType(), "grass") && Objects.equals(enemy.getType(), "fire")) {
+            tempAttack /= 2;
         }
-        else if (Objects.equals(myType, "earth") && Objects.equals(enemy.myType, "water")) {
-            tempStat = myAttackStat * 2;
+        else if (Objects.equals(getType(), "grass") && Objects.equals(enemy.getType(), "water")) {
+            tempAttack *= 2;
         }
 
-        if (tempStat == enemy.myDefenceStat) {
+        // If the attackStat is the same then the enemy's defenceStat after checking the effectivity
+        // the enemy Infomon gets transformed and the method is executed again
+        if (tempAttack == enemy.myDefenceStat) {
             enemy.transform();
             isStronger(enemy);
         }
 
-        return tempStat > enemy.myDefenceStat;
+        return tempAttack > enemy.getDefenceStat();     // returning who is stronger
     }
 
+    // Checks if Infomon is weaker than enemy Infomon
     public boolean isWeaker(Infomon enemy) {
-        int tempStat = enemy.myAttackStat;
-//        if (Objects.equals(myType, "fire") && Objects.equals(enemy.myType, "fire")) {
-//
-//        }
-        if (Objects.equals(myType, "fire") && Objects.equals(enemy.myType, "earth")) {
-            tempStat = myAttackStat * 2;
+        int enemyTempAttack = enemy.getAttackStat();    // storing enemy's attackStat in temp variable so actual value isn't manipulated.
+
+        // Checking all different cases of effectiveness and changing the temporary attackStat accordingly
+        if (Objects.equals(getType(), "fire") && Objects.equals(enemy.getType(), "grass")) {
+            enemyTempAttack *= 2;
         }
-        else if (Objects.equals(myType, "fire") && Objects.equals(enemy.myType, "water")) {
-            tempStat = myAttackStat / 2;
+        else if (Objects.equals(getType(), "fire") && Objects.equals(enemy.getType(), "water")) {
+            enemyTempAttack /= 2;
         }
-//        if (Objects.equals(myType, "water") && Objects.equals(enemy.myType, "water")) {
-//
-//        }
-        else if (Objects.equals(myType, "water") && Objects.equals(enemy.myType, "fire")) {
-            tempStat = myAttackStat * 2;
+        else if (Objects.equals(getType(), "water") && Objects.equals(enemy.getType(), "fire")) {
+            enemyTempAttack *= 2;
         }
-        else if (Objects.equals(myType, "water") && Objects.equals(enemy.myType, "earth")) {
-            tempStat = myAttackStat / 2;
+        else if (Objects.equals(getType(), "water") && Objects.equals(enemy.getType(), "grass")) {
+            enemyTempAttack /= 2;
         }
-//        if (Objects.equals(myType, "earth") && Objects.equals(enemy.myType, "earth")) {
-//            myAttackStat = myAttackStat;
-//        }
-        else if (Objects.equals(myType, "earth") && Objects.equals(enemy.myType, "fire")) {
-            tempStat = myAttackStat / 2;
+        else if (Objects.equals(getType(), "grass") && Objects.equals(enemy.getType(), "fire")) {
+            enemyTempAttack /= 2;
         }
-        else if (Objects.equals(myType, "earth") && Objects.equals(enemy.myType, "water")) {
-            tempStat = myAttackStat * 2;
+        else if (Objects.equals(getType(), "grass") && Objects.equals(enemy.getType(), "water")) {
+            enemyTempAttack *= 2;
         }
-        return tempStat > myDefenceStat;
+        return enemyTempAttack > getDefenceStat();      // Returning who is weaker
     }
 
-
+    // Transforms the Infomon
     public void transform() {
         System.out.println("Transformation!!");
-        switch (myType) {
-            case "earth":
+        switch (getType()) {    // used enhanced switch statement so no breaks are necessary
+            case "grass" -> {
                 myAttackStat += 200;
                 myDefenceStat += 200;
-            case "water":
+            }
+            case "water" -> {
                 myAttackStat += 100;
                 myDefenceStat += 300;
-            case "fire":
+            }
+            case "fire" -> {
                 myAttackStat += 300;
                 myDefenceStat += 100;
+            }
+            default -> {
+                System.out.println("Unknown type!");
+            }
         }
     }
 }
